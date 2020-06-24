2a83ab109386efdfa33bf50d0c538a703f182bc2
rijamyazid <rijamyazid@gmail.com>
2020-06-24 22:22:54 +0700

    ALELEL

--- page_one.php
+++ page_one.php
@@ -1,6 +1,6 @@
 <div class="container-fluid">
     <div class="row">
-        <div class="col-4" style="background:red">A</div>
+        <div class="col-4" style="background:red">ALELELELE</div>
         <div class="col-4" style="background:green">B</div>
         <div class="col-4" style="background:blue">C</div>
     </div>
