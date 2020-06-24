5f54fc819b8d20164315fb0780ba5d6b1a0d72dd
rijamyazid <rijamyazid@gmail.com>
2020-06-24 21:51:21 +0700

    PERUBAHAN : Mengubah banyaknya kolom di page_one

--- page_one.php
+++ page_one.php
@@ -1,3 +1,7 @@
-<div class="container-fluid" style="background:blue">
-    Page One
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-4" style="background:red">A</div>
+        <div class="col-4" style="background:green">B</div>
+        <div class="col-4" style="background:blue">C</div>
+    </div>
 </div>
